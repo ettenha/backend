52e43b9734a5c4dd39abce7cc65979560c744aa2
ettenha <jmterrat@gmail.com>
2026-01-09 12:51:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-import uuid, shutil
+import shutil
+import uuid
 import uvicorn
 from ocr import extract_stats
 
 app = FastAPI()
 
+# CORS OBLIGATOIRE
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 @app.post("/extract")
 async def extract(file: UploadFile = File(...)):
-    path = f"/tmp/{uuid.uuid4()}.png"
-    with open(path, "wb") as buffer:
+    filename = f"/tmp/{uuid.uuid4()}.png"
+
+    with open(filename, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    return extract_stats(path)
+    result = extract_stats(filename)
+    return result
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
